3636d60921104d5cd4da82e6c824b0258a092685
Isaac Lutz <ilutz@MacBook-Pro-122.local>
2021-12-02 14:32:40 -0800

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='protomer-network-distributions',
+    version='0.1',    
+    url='https://github.com/idlutz/protomer-network-distributions',
+    author='Isaac Lutz',
+    author_email='ilutz@uw.edu',
+    packages=['protomer-network-distributions'],
+    install_requires=['numpy', 'libroadrunner', 'pandas','tellurium','matplotlib'],
+)
