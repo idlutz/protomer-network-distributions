625725fdf5ca35ada21746424c9fd29a98298aab
Isaac Lutz <ilutz@MacBook-Pro-122.local>
2021-12-02 16:03:00 -0800

    renamed file, deleted

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     author='Isaac Lutz',
     author_email='ilutz@uw.edu',
     packages=['protomer-network-distributions'],
-    install_requires=['numpy', 'libroadrunner', 'pandas','tellurium','matplotlib'],
+    install_requires=['numpy','pandas','tellurium','matplotlib','seaborn']
 )
