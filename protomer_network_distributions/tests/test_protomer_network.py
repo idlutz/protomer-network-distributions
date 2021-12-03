080cf6236b25d0bc09cda4a6d4c28091fcb92f0a
Isaac Lutz <ilutz@MacBook-Pro-122.local>
2021-12-03 10:37:21 -0800

    updated protomer network test

--- protomer_network_distributions/tests/test_protomer_network.py
+++ protomer_network_distributions/tests/test_protomer_network.py
@@ -5,16 +5,18 @@ import tellurium as te
 import unittest
 from protomer_network_distributions import protomer_network
 
+
+TEST_INPUT_DICT = {
+			'A_B':10*10**-9,
+			'A_C':100*10**-9,
+			'B_C':1*10**-9
+		  }
+
+
 class Test_protomer_network(unittest.TestCase):
 	"""Unit test for protomer_network class"""
 	def test___init___(self):
-		input_dict = {
-						'A_B':10*10**-9,
-						'A_C':100*10**-9,
-						'B_C':1*10**-9
-					 }
-
-		self.test_class = protomer_network(input_dict)
+		self.test_class = protomer_network(TEST_INPUT_DICT)
 		self.assert(self.test_class.protomer_dict is not None)
 		self.assert(self.test_class.simulations is not None)
 		self.assert(self.test_class.simulation_distributions is not None)
